59c57ca74a8a3266615240ccb80106a4f22fba20
Chemi <abbyandam@gmail.com>
2022-10-23 16:01:31 -0400

    updated home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import * as React from "react"
 import * as headerStyles from "./headerStyles.scss"
+// import Logo from "./Logo.js"
 
 const Header = () => {
   return (
-    <div style={headerStyles.header} class="header">
-      <h1> Home </h1>
-      <h1> About </h1>
-      <h1> amongus </h1>
+    <div>
+      {/* <div class="logo">
+        <Logo/>
+  </div> */}
+
+      <div class="header" style={headerStyles.header}>
+        <h1> Home </h1>
+        <h1> Team </h1>
+        <h1> PARC ACADIA #22070 </h1>
+        <h1> Contact </h1>
+        <h1> About </h1>
+      </div>
     </div>
   )
 }
---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const Logo = () => {
+    return(
+        <div>
+            <svg>
+                <circle/>
+            </svg>
+        </div>
+    )
+}
+
+export default Logo
